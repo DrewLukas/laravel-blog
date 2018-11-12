b8f82500f8f8f7a2df294fb1e0dde37aed0706d2
Jasper <j.drewes@telfort.nl>
2018-11-12 16:34:37 +0100

    update layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
         
     </head>
     <body>
-        <h1>Welcome to Laravel</h1>
-        <p>This is the Laravel Application from the "Laravel From Scratch" YouTube series.</p>
+        @yield('content')
     </body>
 </html>
