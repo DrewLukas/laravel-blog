e513b581e1631a27feed27fc2163b09ff42bb19e
Jasper <j.drewes@telfort.nl>
2018-11-13 16:18:44 +0100

    styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
-        <title>{{config('app.name', 'Laravel Project Jasper')}}</title>
+        <title>{{config('app.name', 'Laravel JD')}}</title>
         
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
