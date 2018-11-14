324e1a9be76475d7a10ea16b40729d585d5559c6
Jasper <j.drewes@telfort.nl>
2018-11-14 14:29:00 +0100

    explore including public app.js in layout blade file, making Bootstrap JavaScript functionality works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        {{-- <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">  dit is een link naar een zelf aangemaakte
+        bootstrap.css min file, gehaald van https://www.bootstrapcdn.com/--}}
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
         <title>{{config('app.name', 'Laravel JD')}}</title>
         
@@ -13,5 +15,11 @@
         <div class="container">
             @yield('content')
         </div>
+        <!-- Optional JavaScript (code gehaald van www.getbootstrap.com -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> --}}
+    <script src="{{asset('js/app.js')}}"></script>
     </body>
 </html>
