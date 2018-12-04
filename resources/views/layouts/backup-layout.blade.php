eb03fa6a7116e35cea6f5af3530e72e61718f9ba
Jasper <j.drewes@telfort.nl>
2018-12-04 14:32:48 +0100

    edit layout app.blade.php, backup aangemaakt

---
+++ resources/views/layouts/backup-layout.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        {{-- <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">  dit is een link naar een zelf aangemaakte
+        bootstrap.css min file, gehaald van https://www.bootstrapcdn.com/--}}
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <title>{{config('app.name', 'Laravel JD')}}</title>
+        
+    </head>
+    <body>
+        @include('inc.navbar')
+        <div class="container">
+            @include('inc.messages')
+            @yield('content')
+        </div>
+        <!-- Optional JavaScript (code gehaald van www.getbootstrap.com -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> --}}
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+    </body>
+</html>
